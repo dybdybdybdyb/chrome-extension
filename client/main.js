40175a4db29827c50919b2b9f24f253bf24c0312
Allison <47452487+allisons11@users.noreply.github.com>
2021-07-22 10:46:40 -0400

    Remove calendar classes from main.js

--- client/main.js
+++ client/main.js
@@ -1,45 +1,6 @@
-class Calendar {
-  //do something with the data here
-  constructor(schedule) {
-
-  }
-}
-
-class Event {
-  constructor(data) {
-    this.startTime = data.startTime;
-    this.endTime = data.endTime;
-    this.description = data.description;
-  }
-}
-
-class Day {
-  constructor(name) {
-    this.name = name;
-    this.events = [];
-    this.addEvent = (eventData) => {
-      this.events.push(new Event(eventData));
-    }
-    this.orderEvents = () => {
-      //go through events and sort them by start and end time
-    }
-  }
-}
-
-class Week {
-  constructor(number) {
-    this.days = [];
-    this.addDay = (name) => {
-      const day = new Day(name);
-      this.days.push(day);
-    }
-  }
-}
-
-
 document.addEventListener('DOMContentLoaded', () => {
   const title = document.createElement('h1');
-  title.innerText = 'Social Calendar';
+  title.innerText = 'Online Chatroom';
   document.querySelector('body').appendChild(title);
   // make AJAX call here....
 });
